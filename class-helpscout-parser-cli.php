a11c9176bfccf2c78e8617d9c9f4fdedee72c56a
Alessandro Tesoro <mythemesdepot@gmail.com>
2016-08-20 14:59:17 +0200

    verify api key

--- class-helpscout-parser-cli.php
+++ class-helpscout-parser-cli.php
@@ -8,25 +8,16 @@
  * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU LESSER GENERAL PUBLIC LICENSE
 */
 
+use HelpScoutDocs\DocsApiClient;
+
 class HelpScout_Parser_CLI extends WP_CLI_Command {
 
-  /**
+	/**
 	 * Generate an offline documentation for the current theme.
-	 *
-	 * ## OPTIONS
-	 *
-	 * <folder_name>
-	 * : Plugin's folder name. Example: jetpack
-	 *
-	 * <previous_version>
-	 * : Plugin's previous version.
-	 *
-	 * <new_version>
-	 * : Plugin's new version.
 	 *
 	 * ## EXAMPLES
 	 *
-	 *     wp helpscout generate test
+	 *     wp helpscout generate
 	 *
 	 * @access		public
 	 * @param		  array $args
@@ -35,7 +26,26 @@ class HelpScout_Parser_CLI extends WP_CLI_Command {
 	 */
 	public function generate( $args, $assoc_args ) {
 
-  }
+		$helpscout_api_key = $this->get_api_key();
+
+		if( ! $helpscout_api_key ) {
+			WP_CLI::error( esc_html__( 'Could not find an api key, please define a constant with the name WPCLI_HELPSCOUT_DOCS_KEY to set your api key.' ) );
+		}
+
+	}
+
+	/**
+	 * Get the API KEY.
+	 *
+	 * @return string api key.
+	 */
+	private function get_api_key() {
+
+		$key = ( defined( 'WPCLI_HELPSCOUT_DOCS_KEY' ) ) ? WPCLI_HELPSCOUT_DOCS_KEY : false;
+
+		return $key;
+
+	}
 
 }
 
