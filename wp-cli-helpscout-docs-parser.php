2a865d2d1a94517aa3ed5e1d28f4a8225eb61f8b
Alessandro Tesoro <mythemesdepot@gmail.com>
2016-08-20 13:18:16 +0200

    Hello!

---
+++ wp-cli-helpscout-docs-parser.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WP-CLI Help Scout Docs Parser
+Plugin URI:  https://alessandrotesoro.me
+Description: Extract Help Scout docs to create offline documentation.
+Version: 1.0.0
+Author:      Alessandro Tesoro
+Author URI:  http://alessandrotesoro.me
+License:     GPLv2+
+*/
